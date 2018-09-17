27c9ed5c8aa800578ebfa6105811eb8044fdad9f
Charlie <carlosperisalemany@gmail.com>
2018-09-17 18:43:38 -0300

    modificacion en app.js

--- 01-hola-mundo/app.js
+++ 01-hola-mundo/app.js
@@ -2,10 +2,3 @@ let nombre = 'Charlie';
 
 //console.log('Hola' + nombre);
 console.log(`Hola ${nombre}`);
-
-
-
-let nombre2 = 'fulanito';
-
-console.log('Hola' + nombre2);
-console.log(`Hola ${nomebre2}`);
