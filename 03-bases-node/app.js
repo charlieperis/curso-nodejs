7aaaddaa9a0374cd688d656a775c290e0c0556bf
Charlie <carlosperisalemany@gmail.com>
2018-09-28 16:49:26 -0300

    rever clase

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,15 @@
 
 const {crearArchivo } = require ('./multiplicar/multiplicar');
 
-let base = '7';
+//por consola escribimos node app --base=5
+let argv = process.argv; //acá ejecuta el arg  (argumento) de progress
+let parametro = argv[2]; //acá toma el segundo (2) parametro de '--base=5', es decir el '5'
+let base = parametro.split('=')[1] //acá toma el segundo (2) parametro de '--base=5', es decir el '5' *(recordando que siemre en js se cuanta desde el 0)
+
+console.log(base)
+
 
 crearArchivo(base)
     .then(archivo => console.log(`El archivo tablas-del-${base} se creó correctamente`))
     .catch(err => console.log(err))
+
