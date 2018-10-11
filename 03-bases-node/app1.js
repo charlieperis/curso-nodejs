1e26d483c470d39b15796af603efac5aa78d13ee
Charlie <carlosperisalemany@gmail.com>
2018-10-11 09:00:37 -0300

    cambios mínimos

--- 03-bases-node/app1.js
+++ 03-bases-node/app1.js
@@ -12,7 +12,7 @@ const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 let comando = argv._[0];
 
 switch (comando) {
-    case 'listar': //node app1 listar --limite=5 --base 10
+    case 'listar': //node app1 listar --limite=5 --base 10   
         console.log('Listar');
         listarTabla(argv.base, argv.limite);
         break;
